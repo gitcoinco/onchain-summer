34242c8c23e63599057f8252641f9fc530e86b64
Hussein Martinez <husse.dev@gmail.com>
2024-07-02 20:42:07 +0700

    feat: display hero image in main page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,9 @@
+import hero from "@/assets/hero.svg";
+
 export function Home() {
-  return <>Home</>;
+  return (
+    <div className="flex flex-col items-center p-4">
+      <img src={hero} alt="Hero Image" className="h-[600px] w-[622px]" />;
+    </div>
+  );
 }
