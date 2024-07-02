f29f5d7b7f5272a621c8c56d1858351a7eccca28
Hussein Martinez <husse.dev@gmail.com>
2024-07-02 20:41:43 +0700

    feat: set Layout background color changing by path

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,9 +1,17 @@
 import { Navbar } from "@/components/Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
+// To be changed the theme / css
+const backgroundColor: { [key: string]: string } = {
+  "/": "bg-[#FF8744]",
+  "/applications": "bg-[#FF8744]",
+  "/rewards": "",
+};
 export function Layout() {
+  const { pathname } = useLocation();
+
   return (
-    <div>
+    <div className={`h-screen ${backgroundColor[pathname] || ""}`}>
       <Navbar />
       <main>
         <Outlet />
