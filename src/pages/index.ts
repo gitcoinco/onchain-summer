23df2b5e50c8fbb49a72d015e5c59b935cdbc88a
Huss Martinez <husse.dev@gmail.com>
2024-07-02 18:27:51 +0700

    chore: organize react files into folders (#3)
    * feat: set absolute path @/ -> /src
    
    * chore: organize files in folders pages, layouts
    
    * fix(ts errors): move path config from tsconfig.json to tsconfig.app.json

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { Applications } from "./Applications";
+export { Apply } from "./Apply";
+export { Eligibility } from "./Eligibility";
+export { Home } from "./Home";
+export { Rewards } from "./Rewards";
