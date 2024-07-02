75394d2cd388efe6b64be449d0604ec1efd36692
Hussein Martinez <husse.dev@gmail.com>
2024-07-02 20:05:53 +0700

    feat: updated the Layout with the Navbar component

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,34 +1,13 @@
+import { Navbar } from "@/components/Navbar";
 import { Outlet } from "react-router-dom";
 
 export function Layout() {
   return (
-    <main>
-      <Outlet />
-    </main>
+    <div>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+    </div>
   );
-
-  // kepts for reference
-  // return (
-  //   <div>
-  //     <nav>
-  //       <ul>
-  //         <li>
-  //           <Link to="/">Home</Link>
-  //         </li>
-  //         <li>
-  //           <Link to="/eligibility">Eligibility</Link>
-  //         </li>
-  //         <li>
-  //           <Link to="/apply">Apply</Link>
-  //         </li>
-  //         <li>
-  //           <Link to="/applications">Applications</Link>
-  //         </li>
-  //       </ul>
-  //     </nav>
-  //     <main>
-  //       <Outlet />
-  //     </main>
-  //   </div>
-  // );
 }
