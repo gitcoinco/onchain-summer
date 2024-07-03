bfb66c883d31022ebd07dc8100b718313453e28e
Hussein Martinez <husse.dev@gmail.com>
2024-07-03 19:23:21 +0700

    refactor: moved useApplications hook to separate folder

--- src/pages/Applications.tsx
+++ src/pages/Applications.tsx
@@ -1,56 +1,7 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo } from "react";
 import { Column, Table } from "@/components/Table";
 import loadingImg from "@/assets/loading.gif";
-
-type Application = {
-  metadata: {
-    name: string;
-  };
-  status: string;
-};
-
-const API_URL = "https://ezrf-impact.vercel.app/api/trpc/";
-
-const encodeInput = (json: unknown = {}) =>
-  encodeURIComponent(JSON.stringify({ json }));
-
-const callApi = async (path: string) =>
-  fetch(API_URL + path, {
-    headers: {
-      "content-type": "application/json",
-      "round-id": "on-chain-summer-test",
-    },
-  });
-
-function useApplications() {
-  const [error, setError] = useState<Error | undefined>(undefined);
-  const [loading, setLoading] = useState(true);
-  const [applications, setApplications] = useState<Application[]>([]);
-
-  useEffect(() => {
-    callApi(
-      "projects.list?input=" +
-        encodeInput({
-          orderBy: "time",
-          sortOrder: "asc",
-          limit: 10,
-          skip: 0,
-        })
-    ).then((res) => {
-      if (res.ok) {
-        res.json().then((data) => {
-          setApplications(data.result.data.json as Application[]);
-        });
-      } else {
-        setError(new Error("Failed to fetch applications"));
-      }
-
-      setLoading(false);
-    });
-  }, []);
-
-  return { error, loading, applications };
-}
+import { useApplications } from "@/hooks/useApplications";
 
 interface ApplicationRow {
   name: string;
@@ -104,12 +55,12 @@ export function Applications() {
   );
 
   return (
-    <div className={"xl:w-[1280px] min-w-[956px] m-auto"}>
+    <div className={"w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"}>
       <h2 className="text-2xl mb-4">
         All applications {!error && !loading && `(${applications.length})`}
       </h2>
 
-      <div className="px-4 py-6 rounded-3xl bg-white-40 applications-container">
+      <div className="px-4 py-6 rounded-3xl bg-white-40">
         {loading && (
           <div className="text-center">
             <img
