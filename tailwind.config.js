4745b4c8b37c77a3ad4fe55b60eec6bcedfc756b
Hussein Martinez <husse.dev@gmail.com>
2024-07-03 18:14:49 +0700

    feat: extended TW colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "blue-gradient":
+          "linear-gradient(180deg, rgba(182,233,251,1) 0%, rgba(212,255,255,1) 100%)",
+      },
+      colors: {
+        "white-40": "rgba(255, 255, 255, 0.4)",
+        "white-50": "rgba(255, 255, 255, 0.5)",
+        "gray-light": "#dbdbdb",
+        "orange-sunset": "#FF8744",
+      },
+    },
   },
   plugins: [],
 };
