33e0feaa39536106a00f68c4626b2ad6421f8a7d
Hussein Martinez <husse.dev@gmail.com>
2024-07-05 14:51:26 +0700

    chore: remove semicolon in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import hero from "@/assets/hero.svg";
 export function Home() {
   return (
     <div className="flex flex-col items-center p-4">
-      <img src={hero} alt="Hero Image" className="h-[600px] w-[622px]" />;
+      <img src={hero} alt="Hero Image" className="h-[600px] w-[622px]" />
     </div>
   );
 }
