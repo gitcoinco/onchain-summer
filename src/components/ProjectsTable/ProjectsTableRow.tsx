174664b5ce52da3080d0673044ddda934f6d51fc
Hussein Martinez <husse.dev@gmail.com>
2024-09-06 03:54:45 +0700

    chore: render avatar image in row

--- src/components/ProjectsTable/ProjectsTableRow.tsx
+++ src/components/ProjectsTable/ProjectsTableRow.tsx
@@ -1,40 +1,36 @@
 // import clsx from "clsx";
-import { ProjectWithMetrics } from "@/services/ezrfApi/types";
+import { ProjectWithRank } from "@/services/ezrfApi/types";
 // import { Badge } from "./Badge";
 import { getMetrics } from "@/services/metrics";
 
 interface ProjectsTableRowProps {
   index: number;
-  project: ProjectWithMetrics;
+  project: ProjectWithRank;
 }
 
 export function ProjectsTableRow({ project }: ProjectsTableRowProps) {
   // const oddRow = index % 2 !== 0;
 
   return (
-
-    <tr className="text-header-title"
-      key={project.id}
-    >
+    <tr className="text-header-title" key={project.id}>
       {/* <td scope="row">
         <div className="px-12 text-sm">
         {project.rank}
         </div>
       </td> */}
 
-      <td scope="row" className={"px-6 py-2 md:sticky md:left-0 bg-black font-bold text-lg"}>
-
+      <td
+        scope="row"
+        className={"px-6 py-2 md:sticky md:left-0 bg-black font-bold text-lg"}>
         <div className="flex items-center">
-
           <img
-            src={project.profile.profileImageUrl}
+            src={project.metadata?.sunnyAwards?.avatarUrl}
             alt={project.name}
             className="mr-2 border border-black rounded-full size-6 shrink-0"
           />
           <span
             className="font-normal truncate text-lg/10"
-            title={project.name}
-          >
+            title={project.name}>
             {project.name}
           </span>
         </div>
@@ -44,15 +40,13 @@ export function ProjectsTableRow({ project }: ProjectsTableRowProps) {
         <td key={index} className={"px-12 text-sm"}>
           <div className="">
             <span className="">
-              {project.metrics[metric] ? project.metrics[metric].toFixed(2).toString() : '0'}
+              {project.metrics?.[metric]
+                ? project.metrics[metric].toFixed(2).toString()
+                : "0"}
             </span>
           </div>
         </td>
       ))}
-
- 
-
     </tr>
-
   );
 }
