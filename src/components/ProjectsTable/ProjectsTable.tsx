07e27de90f61692de441173199a813fed7133295
Hussein Martinez <husse.dev@gmail.com>
2024-09-10 12:31:39 +0700

    chore: moved isPending isError logic to ProjectsTable, removed LeaderboardTabView

--- src/components/ProjectsTable/ProjectsTable.tsx
+++ src/components/ProjectsTable/ProjectsTable.tsx
@@ -1,41 +1,48 @@
 import { ProjectsTableRow } from "./ProjectsTableRow";
 import { ProjectsTableHeadRow } from "./ProjectsTableHeadRow";
 import { useProjectsContext } from "@/contexts/projectsContext";
-import { useEffect, useState } from "react";
+import loadingImg from "@/assets/loading.gif";
 
 type TableProps = {
   filter: string;
 };
 
 export function ProjectsTable(props: TableProps) {
-  const { projects } = useProjectsContext();
-  const [filtered, setFiltered] = useState(projects);
+  const { projects, isPending, isError } = useProjectsContext();
 
-  useEffect(() => {
-     
-    const approved = projects.filter((project) => project.status === "approved");
-
-    const typed = approved.filter((project) => project.metadata.sunnyAwards.projectType === props.filter);
-    
-    setFiltered(typed);
-
+  if (isPending) {
+    return (
+      <div className="text-center">
+        <img
+          src={loadingImg}
+          alt="Loading..."
+          style={{ height: 20, margin: "0 auto" }}
+        />
+      </div>
+    );
   }
-    , [projects]);
 
-
+  if (isError) {
+    return <div className="text-center">Something went wrong</div>;
+  }
 
   return (
     <div className="relative pt-12 overflow-x-auto">
       <table className="w-full text-xl text-black border-collapse">
         <ProjectsTableHeadRow />
         <tbody>
-          {filtered.map((project, index) => (
-            <ProjectsTableRow
-              key={project.id}
-              index={index}
-              project={project}
-            />
-          ))}
+          {projects
+            .filter(
+              project =>
+                project.metadata.sunnyAwards.projectType === props.filter
+            )
+            .map((project, index) => (
+              <ProjectsTableRow
+                key={project.id}
+                index={index}
+                project={project}
+              />
+            ))}
         </tbody>
       </table>
     </div>
