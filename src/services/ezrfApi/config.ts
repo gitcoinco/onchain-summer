5ed28acff0f02b7ce56d2506cca8bdc09deb242c
gnomadic <edflem+github@gmail.com>
2024-09-16 15:35:35 -0400

    back to prod

--- src/services/ezrfApi/config.ts
+++ src/services/ezrfApi/config.ts
@@ -1,23 +1,15 @@
-// const localUrl = "http://localhost:3000";
 const productionUrl = "https://ezrf-impact.vercel.app";
 
+export const EZRF_API_URL = `${productionUrl}/api/trpc/`;
+// export const EZRF_API_URL = `http://localhost:3000/api/trpc/`;
 
-// export const EZRF_API_URL = `${productionUrl}/api/trpc/`;
-
-export const EZRF_API_URL = `http://localhost:3000/api/trpc/`;
-
-// export const SHARE_URL = "https://metrics.thesunnyawards.fun/";
-export const SHARE_URL = "http://localhost:3000/";
+export const SHARE_URL = "https://metrics.thesunnyawards.fun/";
+// export const SHARE_URL = "localhost:3000/";
 
 type Round = {
   id: string;
   key: string;
 };
-
-// const testRound: Round = {
-//   id: "round-huss-3",
-//   key: "b16ad95e191a16ecfb9a9ef6f9c3599fec7718ffd237d3fecef52a9390b1dd65",
-// };
 
 const sunnyRound: Round = {
   id: "the-sunnys",
