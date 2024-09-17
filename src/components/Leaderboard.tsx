f49737491e49b20272798c4db8cdcc9dcd6455a3
gnomadic <edflem+github@gmail.com>
2024-09-17 10:38:49 -0400

    reducing top padding

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -10,6 +10,7 @@ import { ProjectWithRank } from "@/services/ezrfApi/types";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useProjectsContext } from "@/contexts/projectsContext";
+import { InfoIcon } from "lucide-react";
 
 
 export function Leaderboard() {
@@ -37,7 +38,7 @@ export function Leaderboard() {
         style={{ width: "100%", margin: "0 auto" }}
       />
 
-      <div className="py-32 lg:py-52 ">
+      <div className="py-32 ">
         <div className="absolute left-0 w-4/5 top-36 md:top-56 md:w-2/5">
           <Image
             src={hero}
@@ -49,6 +50,8 @@ export function Leaderboard() {
 
       <div className="sticky z-10 bg-black rounded-md">
         <div className="px-6 lg:px-8 ">
+      
+          
           <Tabs className="pt-12">
             <TabList>
               <Tab>App</Tab>
@@ -64,26 +67,51 @@ export function Leaderboard() {
                 isError={isError} />
             </TabPanel>
             <TabPanel>
-              <ProjectsTable
+              <div className="text-2xl text-center pt-44">
+                Coming Soon!
+              </div>
+              {/* <ProjectsTable
                 filter="creator"
                 onRowClick={handleViewerClick} 
                 projects={projects}
                 isPending={isPending}
-                isError={isError} />
+                isError={isError} /> */}
             </TabPanel>
             <TabPanel>
               <ProjectsTable
                 filter="other"
-                onRowClick={handleViewerClick} 
+                onRowClick={handleViewerClick}
                 projects={projects}
                 isPending={isPending}
                 isError={isError} />
             </TabPanel>
           </Tabs>
         </div>
-        <div className="pb-12 text-xl text-center text-white pt-36 font-sora ">
-          We'll be refreshing data weekly, come back and check!
-        </div>
+
+
+
+    
+        <div className="flex flex-col items-start gap-2 p-12 text-sm text-white">
+            <div className="flex items-center gap-2">
+              <InfoIcon className="w-4 h-4" />
+              <p>Metrics compiled based on open datasets sourced from Dune, Flipside, Goldsky, and Open Source Observer.</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <InfoIcon className="w-4 h-4" />
+              <p>Metrics are refreshed weekly, come back and check for updates.</p>
+            </div>
+
+            <div className="flex items-center gap-2">
+              <InfoIcon className="w-4 h-4" />
+              <p>Don't see your project or think the numbers are off? Please click here to edit your application. The address you provide will be queried against transactions, traces, and logs datasets for relevant metrics.
+              </p>
+            </div>
+
+
+
+
+          </div>
+
       </div>
       {isDetailsOpen ? (
         <>
