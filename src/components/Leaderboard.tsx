5059d885e07c4d600b58edf5639c2c48697d1a81
gnomadic <edflem+github@gmail.com>
2024-09-17 10:40:08 -0400

    padding around coming soon

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -67,7 +67,7 @@ export function Leaderboard() {
                 isError={isError} />
             </TabPanel>
             <TabPanel>
-              <div className="text-2xl text-center pt-44">
+              <div className="text-2xl text-center py-44">
                 Coming Soon!
               </div>
               {/* <ProjectsTable
